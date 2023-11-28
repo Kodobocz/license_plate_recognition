8eebfdc9bc4b8a168cd1d0fc39fda701f3b39e14
Nemes Dominik <nemesdominik@gmail.com>
2023-11-28 10:17:16 +0100

    Done

--- main.py
+++ main.py
@@ -1,55 +1,47 @@
 import cv2
 import imutils
-import pytesseract
-
-from functions import TestWaitKey
-
-#Download and set route
-#https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.3.0.20221214.exe
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 #Reading and resizing image
-image = cv2.imread(".\data\image1.jpg")
-image = imutils.resize(image, width=500)
-cv2.imshow("Original Image", image)
-TestWaitKey()
+image_number = input("Enter the number of the image: ")
+file_path = f"./data/image{image_number}.jpg"
+originalimage = cv2.imread(file_path)
+originalimage = imutils.resize(originalimage, width=500)
+cv2.imshow("Original Image", originalimage)
+cv2.waitKey(0)
 
 # GrayScaling the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray Scale Image", gray)
-TestWaitKey()
+gray_image = cv2.cvtColor(originalimage, cv2.COLOR_BGR2GRAY) #->Extracting the luminance of the colors, so we only have the intensity of light in black and white
+cv2.imshow("Gray Scale Image", gray_image)
+cv2.waitKey(0)
 
-# Smoothing the image to ignore unimportant contours
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-cv2.imshow("Smoother Image", gray)
-TestWaitKey()
+# Smoothing the image to "ignore" unimportant contours and getting rid of noise
+gray_image_smooth = cv2.bilateralFilter(gray_image, 11, 17, 17)
+cv2.imshow("Smoother Image", gray_image)
+cv2.waitKey(0)
 
-# Make edges more defined  -->szakadozottsághoz állít, alacsony treshold alatti off, 2treshold között akkor, ha összeköt 2 erős kontúrt, semmi közepén eldob
-edged = cv2.Canny(gray, 100, 150)
-cv2.imshow("Canny edge", edged)
-TestWaitKey()
+# Getting the edges
+edged_image = cv2.Canny(gray_image_smooth, 100, 150) #under 100-> edge if connected to two strong edges, over 150->strong edges
+cv2.imshow("Canny edge", edged_image)
+cv2.waitKey(0)
 
 # Finding the contours
-cnts, new = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts, new = cv2.findContours(edged_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #CHAIN.. ->conserves only the end points of a straight edge
 
 # Drawing the contours on the main image
-image1 = image.copy()
-cv2.drawContours(image1, cnts, -1, (0, 255, 0), 3)
-cv2.imshow("Canny After Contouring", image1)
-TestWaitKey()
+contoured_image = originalimage.copy()
+cv2.drawContours(contoured_image, cnts, -1, (0, 255, 0), 3)
+cv2.imshow("Canny After Contouring", contoured_image)
+cv2.waitKey(0)
 
-# Sorting the top 30 most "contoured" places
+# Sorting the top 30 contourareas
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:30]
 NumberPlateCount = None
 
 # Drawing these on the main image
-image2 = image.copy()
-cv2.drawContours(image2, cnts, -1, (0, 255, 0), 3)
-cv2.imshow("Top 30 Contours", image2)
-TestWaitKey()
-
-count = 0
-name = 1
+sorted_contoured_image = originalimage.copy()
+cv2.drawContours(sorted_contoured_image, cnts, -1, (0, 255, 0), 3)
+cv2.imshow("Top 30 Contours", sorted_contoured_image)
+cv2.waitKey(0)
 
 # Finding the number plate
 for cnt in cnts:
@@ -58,19 +50,16 @@ for cnt in cnts:
     if len(approx) == 4:
         NumberPlateCount = approx
         x, y, w, h = cv2.boundingRect(cnt)
-        crp_img = image[y-10 : y + h+5, x-10 : x + w+5]
-        cv2.imwrite(str(name) + '.png', crp_img)
-        name += 1
-
+        crp_img = originalimage[y-10 : y+h+5 , x-10 : x+w+5]
+        cv2.imwrite('./result/result_'+str(image_number) + '.png', crp_img)
         break
 
-
 # Drawing the contour on the main image
-cv2.drawContours(image, [NumberPlateCount], -1, (0, 255, 0), 3)
-cv2.imshow("Final Image", image)
-TestWaitKey()
+cv2.drawContours(originalimage, [NumberPlateCount], -1, (0, 255, 0), 3)
+cv2.imshow("Final Image", originalimage)
+cv2.waitKey(0)
 
 # Cutting out the number plate
-crop_img_loc = '1.png'
+crop_img_loc = f"./result/result_{image_number}.png"
 cv2.imshow("Cropped Image", cv2.imread(crop_img_loc))
-TestWaitKey()
+cv2.waitKey(0)
